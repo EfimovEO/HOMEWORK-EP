156a301551a8093f7b0c9dbf3a7599d6a56b9aa1
Efimov Evgeney <efimov.evgeney@gmail.com>
2021-04-05 19:41:40 +0300

    Update hw_29-30_task-1.py
    fix homework from pull request remarks

--- python/hw_29-30_task-1.py
+++ python/hw_29-30_task-1.py
@@ -5,9 +5,11 @@ Task 1
 Self-study input() function. Write a script which accepts a sequence of comma-separated numbers from user 
 and generate a list and a tuple with those numbers and prints these objects as-is (just print(list) without any formatting).
 '''
+def gen_list_tuple():
+    x = input("Please, input comma-separated numbers:")
+    ls = x.split(',')
+    print("This is list: {}".format(ls))
+    tp=tuple(ls)
+    print("This is tuple: {}".format(tp))
 
-x = input("Please, input comma-separated numbers:")
-ls = x.split(',')
-print("This is list: {}".format(ls))
-tp=tuple(ls)
-print("This is tuple: {}".format(tp))
+gen_list_tuple()
