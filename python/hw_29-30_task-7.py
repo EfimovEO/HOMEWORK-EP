df6212ebd47a7f0e1cdcbf1ce99d99c17c532481
Efimov Evgeney <efimov.evgeney@gmail.com>
2021-04-05 20:40:59 +0300

    Update hw_29-30_task-7.py
    fix hw from pull requests

--- python/hw_29-30_task-7.py
+++ python/hw_29-30_task-7.py
@@ -13,29 +13,31 @@ Spiral should start from top left cell and has a clockwise direction (see the ex
 13 12 11 10 9
 '''
 
+def spiral():
+    N = int(input("Please, input a size argument:"))
+    
+    # создаем список состоящий из списков (матрицу), который потом будем заполнять
+    a = [None] * N
+    for i in range(N): a[i] = [None] * N
+    
+    #начальные координаты
+    x = 0
+    y = 0
+    
+    # эти переменные задают движение по матрице.
+    dx = 1
+    dy = 0
+    
+    #заполнение матрицы
+    for i in range(N * N):
+        a[y][x] = i + 1
+        ts = x + dx if dx else y + dy
+        if ts < 0 or ts == N or a[y + dy][x + dx] != None:
+            dx, dy = -dy, dx
+        x += dx
+        y += dy
+    
+    #вывод матрицы
+    for y in range(N): print(a[y])
 
-N = int(input("Please, input a size argument:"))
-
-# создаем список состоящий из списков (матрицу), который потом будем заполнять
-a = [None] * N
-for i in range(N): a[i] = [None] * N
-
-#начальные координаты
-x = 0
-y = 0
-
-# эти переменные задают движение по матрице.
-dx = 1
-dy = 0
-
-#заполнение матрицы
-for i in range(N * N):
-	a[y][x] = i + 1
-    ts = x + dx if dx else y + dy
-	if ts < 0 or ts == N or a[y + dy][x + dx] != None:
-		dx, dy = -dy, dx
-	x += dx
-	y += dy
-
-#вывод матрицы
-for y in range(N): print(a[y])
+spiral()
