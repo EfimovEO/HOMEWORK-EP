ab5e6cb0e09d19c5b2bf8e8677a714f59a159a4e
Efimov Evgeney <efimov.evgeney@gmail.com>
2021-04-05 20:33:04 +0300

    Update hw_29-30_task-6.py
    fix hw from pull requests

--- python/hw_29-30_task-6.py
+++ python/hw_29-30_task-6.py
@@ -6,8 +6,10 @@ Create a function that will take a string as an argument and return a dictionary
 where keys are symbols from the string and values are the count of inclusion of that symbol.
 '''
 
+def return_dict():
+    x = input("Please, input a string:")
+    ls = list(x)
+    dictionary={i:ls.count(i) for i in set(ls)}
+    print(dictionary)
 
-x = input("Please, input a string:")
-ls = list(x)
-dictionary={i:ls.count(i) for i in set(ls)}
-print(dictionary)
+return_dict()
