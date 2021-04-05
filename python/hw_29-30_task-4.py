d0320eda5b89494283d6e0095036f5d0c9695931
Efimov Evgeney <efimov.evgeney@gmail.com>
2021-04-05 20:25:44 +0300

    Update hw_29-30_task-4.py
    fix hw from pull requests

--- python/hw_29-30_task-4.py
+++ python/hw_29-30_task-4.py
@@ -13,13 +13,27 @@ to work happily on <ACTUAL_OS_NAME>-<ACTUAL_OS_RELEASE>.
 import os
 import pwd
 
+def pr_format1(pid, usename, OS_NAME, OS_RELEASE):
+    tx='''
+    This script has the following PID: {0}.
+    It was ran by {1}
+    to work happily on {2}-{3}.
+    '''.format(pid, usename, OS_NAME, OS_RELEASE)
+    print(tx)
+    
+def pr_format2(pid, usename, OS_NAME, OS_RELEASE):
+    tx=f'''
+    This script has the following PID: {pid}.
+    It was ran by {usename}
+    to work happily on {OS_NAME}-{OS_RELEASE}.
+    '''
+    print(tx)
+
+
 pid = os.getpid()
 usename=pwd.getpwuid(os.getuid())[0]
 OS_NAME=os.uname()[0]
 OS_RELEASE=os.uname()[2]
-tx='''
-This script has the following PID: {0}.
-It was ran by {1}
-to work happily on {2}-{3}.
-'''.format(pid, usename, OS_NAME, OS_RELEASE)
-print(tx)
+
+pr_format1(pid, usename, OS_NAME, OS_RELEASE)
+pr_format2(pid, usename, OS_NAME, OS_RELEASE)
