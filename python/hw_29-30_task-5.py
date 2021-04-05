efe60d3ee56831557047dcf84d7c1904ef27defc
Efimov Evgeney <efimov.evgeney@gmail.com>
2021-04-05 20:30:33 +0300

    Update hw_29-30_task-5.py
    fix hw from pull requests

--- python/hw_29-30_task-5.py
+++ python/hw_29-30_task-5.py
@@ -9,34 +9,37 @@ If return value will have less than 3 elements for some reason it's ok and shoul
 but some list should be returned in any case.
 '''
 
+def top_three_max_int():
+    x = input("Please, input comma-separated list of integers:")
+    ls = x.split(',')
+    error_ls=[]
+    
+    # этим циклом заполняем список со значениями, которые не int и коныертируем в int числа
+    i=0
+    for l in ls:
+        try:
+            ls[i]=int(l)
+        except ValueError:
+            error_ls.append(l)
+        i+=1
+    
+    # этим циклом удаляем из исходного списка значения, которые не int 
+    for l in error_ls:
+        ls.remove(l)
+    
+    # чтобы оставить уникальные значения сделаем из списка множество    
+    myset = set(ls)
+    # вернем список, чтобы далее можно было его отсортировать
+    ls=list(myset)
+    ls.sort(reverse=True)
+    
+    if len(ls)<3:
+        top=len(ls)
+    else:
+        top=3
+    
+    print("List of top max integers: {}".format(ls[0:top]))
+    print("You've passed some extra elements that I can't parse: {}".format(error_ls))
 
-x = input("Please, input comma-separated list of integers:")
-ls = x.split(',')
-error_ls=[]
 
-# этим циклом заполняем список со значениями, которые не int и коныертируем в int числа
-i=0
-for l in ls:
-    try:
-        ls[i]=int(l)
-    except ValueError:
-        error_ls.append(l)
-    i+=1
-
-# этим циклом удаляем из исходного списка значения, которые не int 
-for l in error_ls:
-    ls.remove(l)
-
-# чтобы оставить уникальные значения сделаем из списка множество    
-myset = set(ls)
-# вернем список, чтобы далее можно было его отсортировать
-ls=list(myset)
-ls.sort(reverse=True)
-
-if len(ls)<3:
-    top=len(ls)
-else:
-    top=3
-
-print("List of top max integers: {}".format(ls[0:top]))
-print("You've passed some extra elements that I can't parse: {}".format(error_ls))
+top_three_max_int()
